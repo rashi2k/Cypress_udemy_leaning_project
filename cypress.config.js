961cfc569d7f5bab440bd8a507bbaf5869290a23
rashini <rashininanayakkara@gmail.com>
2023-03-21 09:07:19 +0530

    Add new changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("cypress");
 //const fs = require('fs')
-const { uniqueRandomCodeGeneratorTask } = require("./cypress/e2e/code_generator");
+const { uniqueRandomCodeGeneratorTask } = require("./cypress/e2e/code_generator_task");
 
 module.exports = defineConfig({
   e2e: {
--- cypress/e2e/Task.cy.js
+++ cypress/e2e/Task.cy.js
@@ -2,8 +2,8 @@
 
 describe('First Suit', () => {
     it('test case 1', () => {
-        cy.task('uniqueRandomCodeGeneratorTask', { moduleName: 'Hello', codeLength:  4 }).then((data) => {
-            cy.log("ddddd" , data)
+        cy.task('uniqueRandomCodeGeneratorTask', { moduleName: 'Hello', codeLength:  4 }).then((code) => {
+            cy.log("generated code" , code)
         })
     })
 
